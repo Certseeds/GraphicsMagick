f4f8e64c691020fd5a107e0e96f57f19c24bba1e
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2023-09-04 09:05:06 -0500

    Tidy Changelog white-space and update generated files.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x272403
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 27,24,3
-#define MagickChangeDate   "20230902"
-#define MagickReleaseDate  "snapshot-20230902"
+#define MagickChangeDate   "20230904"
+#define MagickReleaseDate  "snapshot-20230904"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
