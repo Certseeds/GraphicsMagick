77a7547874325ed8d5e67ca6ea0f01b690832f04
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2023-09-22 18:27:16 -0500

    BMP does not need to depend on additional libraries.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x272403
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 27,24,3
-#define MagickChangeDate   "20230923"
-#define MagickReleaseDate  "snapshot-20230923"
+#define MagickChangeDate   "20230922"
+#define MagickReleaseDate  "snapshot-20230922"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
