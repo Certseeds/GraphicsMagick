e3d7cbf8762057f4f2839da2d7f919d6cdca8227
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2023-11-28 09:13:51 -0600

    configure.ac: Fix many bugs related to building library dependency list

--- fuzzing/oss-fuzz-build.sh
+++ fuzzing/oss-fuzz-build.sh
@@ -3,6 +3,16 @@
 # This is intended to be run from OSS-Fuzz's build environment. We intend to
 # eventually refactor it to be easy to run locally.
 
+# For local testing of fuzzing/oss-fuzz-build.sh do:
+# ~/src/GM% cp fuzzing/oss-fuzz-build.sh ../oss-fuzz-heptapod/projects/graphicsmagick/
+
+enable_heif=false
+enable_xml=false
+
+export PKG_CONFIG_PATH="$WORK/lib/pkgconfig"
+export PKG_CONFIG='pkg-config --static'
+export VERBOSE=TRUE
+
 printf "SRC=${SRC}\n"
 printf "WORK=${WORK}\n"
 printf "OUT=${OUT}\n"
@@ -28,7 +38,8 @@ popd
 printf "=== Building ${SRC}/xz...\n"
 pushd "$SRC/xz"
 ./autogen.sh --no-po4a --no-doxygen
-PKG_CONFIG_PATH="$WORK/lib/pkgconfig" PKG_CONFIG='pkg-config --static' ./configure \
+#PKG_CONFIG_PATH="$WORK/lib/pkgconfig" PKG_CONFIG='pkg-config --static'
+./configure \
                --disable-xz \
                --disable-xzdec \
                --disable-lzmadec \
@@ -59,7 +70,8 @@ printf "=== Building ${SRC}/libpng...\n"
 #    -L/work/lib -lpng16 -lm -lz
 pushd "$SRC/libpng"
 autoreconf -fiv
-PKG_CONFIG_PATH="$WORK/lib/pkgconfig" PKG_CONFIG='pkg-config --static' ./configure \
+#PKG_CONFIG_PATH="$WORK/lib/pkgconfig" PKG_CONFIG='pkg-config --static'
+./configure \
                --prefix="$WORK" \
                --disable-shared \
                --enable-static \
@@ -70,12 +82,15 @@ make -j$(nproc)
 make install
 popd
 
-#printf "=== Building ${SRC}/libxml2...\n"
-#pushd "$SRC/libxml2"
-#./autogen.sh --disable-shared --without-debug --without-legacy --without-python --without-schematron --without-schemas --prefix="${WORK}"
-#make -j$(nproc)
-#make install
-#popd
+if ${enable_xml}
+then
+    printf "=== Building ${SRC}/libxml2...\n"
+    pushd "$SRC/libxml2"
+    ./autogen.sh --disable-shared --without-debug --without-legacy --without-python --without-schematron --without-schemas --prefix="${WORK}"
+    make -j$(nproc)
+    make install
+    popd
+fi
 
 # build libjpeg-turbo
 # PKG_CONFIG_PATH=/work/lib/pkgconfig:/usr/lib/pkgconfig pkg-config --static libjpeg --libs
@@ -98,7 +113,8 @@ popd
 printf "=== Building ${SRC}/libwebp...\n"
 pushd "$SRC/libwebp"
 ./autogen.sh
-PKG_CONFIG_PATH="$WORK/lib/pkgconfig" PKG_CONFIG='pkg-config --static' ./configure \
+#PKG_CONFIG_PATH="$WORK/lib/pkgconfig" PKG_CONFIG='pkg-config --static'
+./configure \
                CPPFLAGS="-I$WORK/include" \
                CFLAGS="$CFLAGS" \
                LDFLAGS="${LDFLAGS:-} -L$WORK/lib" \
@@ -124,19 +140,62 @@ popd
 
 # FXIME: Add libdeflate, and libdeflate build here
 
+# Build libtiff
+# PKG_CONFIG_PATH=/work/lib/pkgconfig:/usr/lib/pkgconfig pkg-config --static libtiff-4 --libs
+#  -L/work/lib -L/usr/local/lib -L/work/lib -ltiff -lwebp -lzstd -llzma -ljpeg -lz -lm -lwebp -lm -lsharpyuv -lm -lzstd -pthread -llzma -pthread -lpthread -ljpeg -lz
+#
+# FIXME: When we link with libtiff we get this error:
+# configure:23717: checking for TIFFOpen in -ltiff
+# configure:23742: clang -o conftest  -O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -fsanitize=address -fsanitize-address-use-after-scope -fsanitize=fuzzer-no-link -Wall  -I/work/include/libpng16 -I/work/include/freetype2 -I/work/include/libxml2 -I/work/include -I/usr/local/include -I/work/include/webp  -L/work/lib -L/usr/local/lib conftest.c -ltiff  -ltiff -lsharpyuv -lwebp -lwebpmux -lfreetype -ljpeg -lturbojpeg -lpng16 -llcms2 -lzstd -llzma -lz -lm  -lpthread >&5
+# /usr/bin/ld: /usr/bin/ld: DWARF error: invalid or unhandled FORM value: 0x25
+# /work/lib/libwebp.a(libwebpencode_la-picture_csp_enc.o): in function `ImportYUVAFromRGBA':
+# picture_csp_enc.c:(.text.ImportYUVAFromRGBA[ImportYUVAFromRGBA]+0x3e2): undefined reference to `SharpYuvInit'
+# /usr/bin/ld: picture_csp_enc.c:(.text.ImportYUVAFromRGBA[ImportYUVAFromRGBA]+0x4b4): undefined reference to `SharpYuvGetConversionMatrix'
+# /usr/bin/ld: picture_csp_enc.c:(.text.ImportYUVAFromRGBA[ImportYUVAFromRGBA]+0x4eb): undefined reference to `SharpYuvConvert'
+# clang-15: error: linker command failed with exit code 1 (use -v to see invocation)
+#
+# but nm on libsharpyuv.a shows that the functions are there!
+
+printf "=== Building ${SRC}/libtiff...\n"
+pushd "$SRC/libtiff"
+autoreconf -fiv
+#PKG_CONFIG_PATH="$WORK/lib/pkgconfig" PKG_CONFIG='pkg-config --static'
+./configure \
+               CPPFLAGS="-I$WORK/include" \
+               CFLAGS="$CFLAGS" \
+               LDFLAGS="${LDFLAGS:-} -L$WORK/lib" \
+               --prefix="$WORK" \
+               --disable-old-jpeg \
+               --disable-shared \
+               --enable-static \
+               --enable-tools \
+               --disable-tests \
+               --disable-contrib \
+               --disable-silent-rules \
+               --disable-ld-version-script
+make -j$(nproc)
+make install
+popd
+
 # Build liblcms2
 # PKG_CONFIG_PATH=/work/lib/pkgconfig:/usr/lib/pkgconfig pkg-config --static lcms2 --libs
 #    -L/work/lib -llcms2 -lm -lpthread
+# liblcms2 does not test for library dependencies using pkg-config!
+# LIBS="`pkg-config --static --libs-only-l zlib libjpeg libtiff-4`"
 printf "=== Building ${SRC}/Little-CMS...\n"
 pushd "$SRC/Little-CMS"
 autoreconf -fiv
-PKG_CONFIG_PATH="$WORK/lib/pkgconfig" PKG_CONFIG='pkg-config --static' ./configure \
+#PKG_CONFIG_PATH="$WORK/lib/pkgconfig" PKG_CONFIG='pkg-config --static'
+./configure \
                CPPFLAGS="-I$WORK/include" \
                CFLAGS="$CFLAGS" \
                LDFLAGS="${LDFLAGS:-} -L$WORK/lib" \
                --prefix="$WORK" \
                --disable-shared \
-               --enable-static
+               --enable-static \
+               --without-jpeg \
+               --without-tiff \
+               --without-zlib
 make -j$(nproc)
 make install
 popd
@@ -147,7 +206,8 @@ popd
 printf "=== Building ${SRC}/freetype...\n"
 pushd "$SRC/freetype"
 ./autogen.sh
-PKG_CONFIG_PATH="$WORK/lib/pkgconfig" PKG_CONFIG='pkg-config --static' ./configure \
+#PKG_CONFIG_PATH="$WORK/lib/pkgconfig" PKG_CONFIG='pkg-config --static'
+./configure \
                CPPFLAGS="-I$WORK/include" \
                CFLAGS="$CFLAGS" \
                LDFLAGS="${LDFLAGS:-} -L$WORK/lib" \
@@ -159,7 +219,10 @@ make -j$(nproc)
 make install
 popd
 
-# Build libx265
+if ${enable_heif} # Start conditional for if libheif related stuff should be included
+then
+
+# Build libx265 (a C++ library)
 # PKG_CONFIG_PATH=/work/lib/pkgconfig:/usr/lib/pkgconfig pkg-config --static x265 --libs
 #    -L/work/lib -lx265 -lc++ -lm -lrt -lm -ldl -lgcc_s -lgcc -lgcc_s -lgcc -lrt -ldl
 pushd "$SRC/x265/build/linux"
@@ -177,7 +240,7 @@ make -j$(nproc) x265-static
 make install
 popd
 
-# Build libde265
+# Build libde265 (a C++ library)
 # PKG_CONFIG_PATH=/work/lib/pkgconfig:/usr/lib/pkgconfig pkg-config --static libde265 --libs
 #    -L/work/lib -lde265 -lstdc++
 printf "=== Building ${SRC}/libde265...\n"
@@ -197,7 +260,7 @@ make -j$(nproc)
 make install
 popd
 
-# Build libaom
+# Build libaom (a mixed C/C++ library)
 # PKG_CONFIG_PATH=/work/lib/pkgconfig:/usr/lib/pkgconfig pkg-config --static aom --libs
 #    -L/work/lib -laom -lm -lpthread
 printf "=== Building ${SRC}/aom...\n"
@@ -227,7 +290,7 @@ make -j$(nproc)
 make install
 popd
 
-# Build libheif
+# Build libheif (a C++ library)
 # PKG_CONFIG_PATH=/work/lib/pkgconfig:/usr/lib/pkgconfig pkg-config --static libheif --libs
 #   -L/work/lib -lheif -lc++ -lde265 -lstdc++ -lx265 -lc++ -lm -lrt -lm -ldl -lgcc_s -lgcc -lgcc_s -lgcc -lrt -ldl -laom -lm -lpthread -ljpeg
 printf "=== Building ${SRC}/libheif...\n"
@@ -237,36 +300,22 @@ rm -rf build
 mkdir build
 cd build
 cmake .. --preset=fuzzing \
+      -DCMAKE_C_COMPILER=$CC \
+      -DCMAKE_CXX_COMPILER=$CXX \
+      -DCMAKE_C_FLAGS="$CFLAGS" \
+      -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
       -DCMAKE_INSTALL_PREFIX=$WORK \
       -DFUZZING_COMPILE_OPTIONS="" \
       -DFUZZING_LINKER_OPTIONS="$LIB_FUZZING_ENGINE" \
       -DFUZZING_C_COMPILER=$CC -DFUZZING_CXX_COMPILER=$CXX \
       -DWITH_DEFLATE_HEADER_COMPRESSION=OFF \
-      -DENABLE_SHARED:bool=off
+      -DENABLE_SHARED:bool=off \
+      -DCONFIG_PIC=1
 make -j$(nproc)
 make install
 popd
 
-# Build libtiff
-# PKG_CONFIG_PATH=/work/lib/pkgconfig:/usr/lib/pkgconfig pkg-config --static libtiff-4 --libs
-#    -L/work/lib -L/usr/local/lib -L/work/lib -ltiff -lwebp -lzstd -llzma -ljpeg -lz -lm -lwebp -lm -lsharpyuv -lm -lzstd -pthread -llzma -pthread -lpthread -ljpeg -lz
-printf "=== Building ${SRC}/libtiff...\n"
-pushd "$SRC/libtiff"
-autoreconf -fiv
-PKG_CONFIG_PATH="$WORK/lib/pkgconfig" PKG_CONFIG='pkg-config --static' ./configure \
-               CPPFLAGS="-I$WORK/include" \
-               CFLAGS="$CFLAGS" \
-               LDFLAGS="${LDFLAGS:-} -L$WORK/lib" \
-               --prefix="$WORK" \
-               --disable-old-jpeg \
-               --disable-shared \
-               --enable-static \
-               --disable-tools \
-               --disable-tests \
-               --disable-contrib
-make -j$(nproc)
-make install
-popd
+fi # End conditional for if libheif related stuff should be included
 
 # Build libjxl
 #  PKG_CONFIG_PATH=/work/lib/pkgconfig:/usr/lib/pkgconfig pkg-config --static libjxl_threads --libs
@@ -331,8 +380,10 @@ ls -l ${WORK}/lib
 
 # freetype-config is in $WORK/bin so we temporarily add $WORK/bin to the path
 # pkg-config names GraphicsMagick, GraphicsMagickWand, GraphicsMagick++
+# -stdlib=libc++'
 printf "=== Building GraphicsMagick...\n"
-PATH=$WORK/bin:$PATH PKG_CONFIG_PATH="$WORK/lib/pkgconfig PKG_CONFIG='pkg-config --static'" ./configure \
+./bootstrap
+PATH=$WORK/bin:$PATH PKG_CONFIG_PATH="$WORK/lib/pkgconfig" PKG_CONFIG='pkg-config --static' ./configure \
     CPPFLAGS="-I$WORK/include/libpng16 -I$WORK/include/freetype2 -I$WORK/include/libxml2 -I$WORK/include" \
     CFLAGS="$CFLAGS" \
     LDFLAGS="${LDFLAGS:-} -L$WORK/lib" \
@@ -350,15 +401,17 @@ set +x
 # maximum extent possible.
 # FIXME: This hard-coded list is not taking advantage of the list that GraphicsMagick configure computed!
 #MAGICK_LIBS="$WORK/lib/libxml2.a $WORK/lib/libjasper.a $WORK/lib/libpng.a $WORK/lib/libtiff.a $WORK/lib/liblcms2.a $WORK/lib/libwebpmux.a $WORK/lib/libwebp.a $WORK/lib/libsharpyuv.a $WORK/lib/libturbojpeg.a $WORK/lib/libfreetype.a $WORK/lib/libzstd.a $WORK/lib/liblzma.a $WORK/lib/libz.a"
-MAGICK_LIBS=''
-for lib in libxml2.a libjasper.a libpng.a libtiff.a liblcms2.a libwebpmux.a libwebp.a libsharpyuv.a libheif.a libturbojpeg.a libfreetype.a libx265.a libde265.a libzstd.a liblzma.a libz.a ; do
-    if [ -f "${WORK}/lib/$lib" ] ; then
-        if [ -n "${MAGICK_LIBS}" ] ; then
-             MAGICK_LIBS="${MAGICK_LIBS} "
-        fi
-        MAGICK_LIBS="${MAGICK_LIBS}${WORK}/lib/${lib}"
-    fi
-done
+#MAGICK_LIBS=''
+#for lib in libxml2.a libjasper.a libpng.a libtiff.a liblcms2.a libwebpmux.a libwebp.a libsharpyuv.a libheif.a libturbojpeg.a libfreetype.a libx265.a libde265.a libzstd.a liblzma.a libz.a ; do
+#    if [ -f "${WORK}/lib/$lib" ] ; then
+#        if [ -n "${MAGICK_LIBS}" ] ; then
+#             MAGICK_LIBS="${MAGICK_LIBS} "
+#        fi
+#        MAGICK_LIBS="${MAGICK_LIBS}${WORK}/lib/${lib}"
+#    fi
+#done
+# GraphicsMagick, GraphicsMagickWand, GraphicsMagick++
+MAGICK_LIBS="$(pkg-config --static --libs-only-L GraphicsMagick++) $(pkg-config --static --libs-only-other GraphicsMagick++) $(pkg-config pkg-config --static --libs-only-l GraphicsMagick++)"
 printf "MAGICK_LIBS=${MAGICK_LIBS}\n"
 
 printf "=== Building fuzzers...\n"
