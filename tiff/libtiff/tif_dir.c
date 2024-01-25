75d44c3474f50de30f84fd48a730d3cb36670c92
Jaroslav Fojtik <devnull@localhost>
2024-01-26 00:55:21 +0100

    tiff/libtiff/tif_dir.c: Apply Su Laus leak fix from: https://gitlab.com/libtiff/libtiff/-/commit/376916d25f39bab69a3ed16963fd3f186870942c

--- tiff/libtiff/tif_dir.c
+++ tiff/libtiff/tif_dir.c
@@ -1679,6 +1679,8 @@ TIFFExtendProc TIFFSetTagExtender(TIFFExtendProc extender)
  */
 int TIFFCreateDirectory(TIFF *tif)
 {
+    /* Free previously allocated memory and setup default values. */
+    TIFFFreeDirectory(tif);
     TIFFDefaultDirectory(tif);
     tif->tif_diroff = 0;
     tif->tif_nextdiroff = 0;
@@ -1691,6 +1693,8 @@ int TIFFCreateDirectory(TIFF *tif)
 
 int TIFFCreateCustomDirectory(TIFF *tif, const TIFFFieldArray *infoarray)
 {
+   /* Free previously allocated memory and setup default values. */
+    TIFFFreeDirectory(tif);
     TIFFDefaultDirectory(tif);
 
     /*
