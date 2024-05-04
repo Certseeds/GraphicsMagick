93e82cfe99081b47c6228cc805c9c59b70f084e1
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2024-05-04 13:57:38 -0500

    Magick++/lib/Magick++/STL.h: Stop using std::unary_function in C++'11 rather than C++'17.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x282500
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 28,25,0
-#define MagickChangeDate   "20240423"
-#define MagickReleaseDate  "snapshot-20240423"
+#define MagickChangeDate   "20240504"
+#define MagickReleaseDate  "snapshot-20240504"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
