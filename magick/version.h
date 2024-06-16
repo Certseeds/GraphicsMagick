75ebf98e256a1855687d703e7ef350c8f2a76638
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2024-06-16 09:07:52 -0500

    configure.ac/url.c: Add additional header file tests.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x282500
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 28,25,0
-#define MagickChangeDate   "20240611"
-#define MagickReleaseDate  "snapshot-20240611"
+#define MagickChangeDate   "20240616"
+#define MagickReleaseDate  "snapshot-20240616"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
