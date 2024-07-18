f2789e8deac1a43f6327c80e1784f72c23cde19d
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2024-07-18 11:21:57 -0500

    _Gm_convert_fp32_to_fp24(): Coverity fixes for arithmetic overflow in mantissa related code.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x282500
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 28,25,0
-#define MagickChangeDate   "20240714"
-#define MagickReleaseDate  "snapshot-20240714"
+#define MagickChangeDate   "20240718"
+#define MagickReleaseDate  "snapshot-20240718"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
