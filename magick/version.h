72636da91a4ba083a97064872b6278dd0db6a131
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2024-08-27 08:04:07 -0500

    1.3.45 is released in 2024

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x282502
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 28,25,2
-#define MagickChangeDate   "20240826"
-#define MagickReleaseDate  "snapshot-20240826"
+#define MagickChangeDate   "20240827"
+#define MagickReleaseDate  "snapshot-20240827"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
