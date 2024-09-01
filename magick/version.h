be586ecccd9a4bee21d915885db09f75b8895bd3
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2024-09-01 12:17:54 -0500

    TclMagick: Update tcl.m4 to TEA_VERSION 3.10 and regen Autotools files

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x282502
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 28,25,2
-#define MagickChangeDate   "20240827"
-#define MagickReleaseDate  "snapshot-20240827"
+#define MagickChangeDate   "20240901"
+#define MagickReleaseDate  "snapshot-20240901"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
