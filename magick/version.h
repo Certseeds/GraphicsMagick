099c24d49feeb2865f4e70f93692ce8ad494d36a
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2024-09-06 16:55:03 -0500

    Options Documentation: Remove #RRRRGGGGBBBBAAAA from the list of supported color specification formats due to conflict.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x282502
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 28,25,2
-#define MagickChangeDate   "20240902"
-#define MagickReleaseDate  "snapshot-20240902"
+#define MagickChangeDate   "20240906"
+#define MagickReleaseDate  "snapshot-20240906"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
