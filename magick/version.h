7bb69108ce1bf3b998e68ff9c8e4cb713ee44f6c
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2024-09-12 10:24:00 -0500

    Validate Validate legacy Ghostscript font file in Ghostscript install directory before deciding to use it.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x282502
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 28,25,2
-#define MagickChangeDate   "20240909"
-#define MagickReleaseDate  "snapshot-20240909"
+#define MagickChangeDate   "20240912"
+#define MagickReleaseDate  "snapshot-20240912"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
