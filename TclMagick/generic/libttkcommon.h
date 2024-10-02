747ee41d5dceb9496cf64d8c7cb1cc767ffadbee
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2024-10-02 09:07:23 -0500

    TclMagick: Eliminate the libttkcommon "shim" library which allowed TkMagick to work and use a different approach. TclMagick is not dependent on libtk.

--- TclMagick/generic/libttkcommon.h
+++ TclMagick/generic/libttkcommon.h
@@ -7,19 +7,47 @@
 #include "config.h"
 #endif /* if defined(HAVE_CONFIG_H) */
 
+#include <assert.h>
 #include "TclMagick.h"
 
+#define TCL_MAGICK_SIGNATURE 0xabacadabUL
+#define TCL_MAGICK_OBJ_VAR "TclMagickContext"
+
 struct TMHT {
+    unsigned long   signature1;  /* Validation signature (head) */
     int             initialized; /* Whether module has been initialized */
     Tcl_HashTable   hashTable;   /* HasTable for MagickWand objects */
+    unsigned long   signature2;  /* Validation signature (tail) */
 };
 typedef struct TMHT TMHT;
+static TMHT* TM_P = (TMHT *) NULL;
 
-EXPORT(TclMagickObj, *findMagickObj)(Tcl_Interp *interp, int type, char *name);
-EXPORT(MagickWand, *findMagickWand)(Tcl_Interp *interp, char *name);
-EXPORT(DrawingWand, *findDrawingWand)(Tcl_Interp *interp, char *name);
-EXPORT(PixelWand, *findPixelWand)(Tcl_Interp *interp, char *name);
-/* EXPORT(int, myMagickError)(Tcl_Interp *interp,MagickWand *wandPtr); */
+
+#if NEED_STATIC_FUNCS
+
+static CONST char* objTypeNames[] = {
+    "wand", "drawing", "pixel", (char *) NULL
+};
+
+static TclMagickObj* findMagickObj(Tcl_Interp *interp, int type, char *name)
+{
+    assert(TM_P->signature1 == TCL_MAGICK_SIGNATURE);
+    assert(TM_P->signature2 == TCL_MAGICK_SIGNATURE);
+    Tcl_HashEntry *hPtr = Tcl_FindHashEntry( &TM_P->hashTable, name );
+    TclMagickObj  *mPtr;
+
+    if( hPtr == NULL ) {
+        Tcl_AppendResult(interp, "Magick object '", name, "' not found", NULL);
+        return (TclMagickObj *)NULL;
+    } else {
+        mPtr = (TclMagickObj *)Tcl_GetHashValue(hPtr);
+        if( (type != TM_TYPE_ANY) && (mPtr->type != type) ) {
+            Tcl_AppendResult(interp,"Magick object '",name,"' is not a ",objTypeNames[type]," object",NULL);
+            return (TclMagickObj *)NULL;
+        }
+        return mPtr;
+    }
+}
 
 static int myMagickError(Tcl_Interp  *interp, MagickWand *wandPtr )
 {
@@ -46,6 +74,9 @@ static int myMagickError(Tcl_Interp  *interp, MagickWand *wandPtr )
      */
     return TCL_ERROR;
 }
+
+#endif /* if NEED_STATIC_FUNCS */
+
 #endif
 
 /* vim: set ts=8 sts=8 sw=8 noet: */
--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x282502
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 28,25,2
-#define MagickChangeDate   "20240929"
-#define MagickReleaseDate  "snapshot-20240929"
+#define MagickChangeDate   "20241002"
+#define MagickReleaseDate  "snapshot-20241002"
 
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
